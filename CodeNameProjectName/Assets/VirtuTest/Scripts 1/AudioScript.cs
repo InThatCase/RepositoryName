4deef6082a49d5e2b3ae03a7e85628d6a1b432cf
Virtuozol <ilikegears3030@gmail.com>
2017-11-14 18:58:11 -0800

    Hey new things.

---
+++ CodeNameProjectName/Assets/VirtuTest/Scripts 1/AudioScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioScript : MonoBehaviour
+{
+
+    public Slider Volume;
+    public AudioSource MyMusic;
+    void Update () { 
+    MyMusic.volume = Volume.value;
+        } 
+
+        
+    }
+ 
+
---
+++ CodeNameProjectName/Assets/VirtuTest/Scripts 1/LoadSceneOnClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+    public void LoadByIndex(int SceneIndex)
+    {
+        SceneManager.LoadScene (SceneIndex);
+    }
+}
+    
+	
+
